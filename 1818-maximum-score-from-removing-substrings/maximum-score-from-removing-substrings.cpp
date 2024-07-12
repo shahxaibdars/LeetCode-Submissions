ebf64352a336f9c6c8b7c70fcff7881ab65e162f
Shahzaib <114687420+shahxaibdars@users.noreply.github.com>
2024-07-12 12:07:52 +0500

    Time: 651 ms (5.29%) | Memory: 20.9 MB (71.95%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,123 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        string ans = s;
+        int res = 0;
+
+
+        // if (y > x){
+        //     while (1){
+        //         int pos = ans.find("ba");
+        //         if (pos != -1){
+        //             ans.erase(pos,2);
+        //             res += y;
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     while (1){
+        //         int pos = ans.find("ab");
+        //         if (pos != -1){
+        //             ans.erase(pos,2);
+        //             res += x;
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // else{
+        //     while (1){
+        //         int pos = ans.find("ab");
+        //         if (pos != -1){
+        //             ans.erase(pos,2);
+        //             res += x;
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     while (1){
+        //         int pos = ans.find("ba");
+        //         if (pos != -1){
+        //             ans.erase(pos,2);
+        //             res += y;
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // 
+        
+        if (x>y){
+            while(1){
+                int count = 0;
+                for (int i = 1; i < s.length(); i++){
+                    if (s[i] == 'b' & s[i-1] == 'a'){
+                        res += x;
+                        s.erase(i-1, 2);
+                        i-=3;
+                        if(i<=0) i=0;
+                        count++;
+                    }
+                }
+                if (count == 0){
+                    break;
+                }
+            }
+            while(1){
+                int count = 0;
+                for (int i = 1; i < s.length(); i++){
+                    if (s[i] == 'a' & s[i-1] == 'b'){
+                        res += y;
+                        s.erase(i-1, 2);
+                        count++;
+                        i-=3;
+                        if(i<=0) i=0;
+                    }
+                }
+                if (count == 0){
+                    break;
+                }
+            }
+        }
+        else{
+            while(1){
+                int count = 0;
+                for (int i = 1; i < s.length(); i++){
+                    if (s[i] == 'a' & s[i-1] == 'b'){
+                        res += y;
+                        s.erase(i-1, 2);
+                        i-=3;
+                        if(i<=0) i=0;
+                        count++;
+                    }
+                }
+                if (count == 0){
+                    break;
+                }
+            }
+            while(1){
+                int count = 0;
+                for (int i = 1; i < s.length(); i++){
+                    if (s[i] == 'b' & s[i-1] == 'a'){
+                        res += x;
+                        s.erase(i-1, 2);
+                        count++;
+                        i-=3;
+                        if(i<=0) i=0;
+                    }
+                }
+                if (count == 0){
+                    break;
+                }
+            }
+        }
+
+        return res;
+
+    }
+};
